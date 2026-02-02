43e3abb8a2d91f0ac051ffee7f3e3d84189d0fb9
NachoSanchezGarcia <ignacio.sanchez@sanoma.com>
2026-02-02 16:29:40 +0100

    feat: Configure Astro for Node.js server output and add & update Docker ignore files.

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
+import node from '@astrojs/node';
 import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
   integrations: [react(), tailwind()],
   server: {
     host: '0.0.0.0',
